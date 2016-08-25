1e4e42bc0c45fbe4de7ca90a85e90720cd174ff8
Jason Addington <jason@vortx.com>
2016-08-24 17:53:43 -0700

    Add the static `ArgumentsProvider` class so clients have a cleaner entry point.

---
+++ src/Clarg/ArgumentsProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Clarg
+{
+	public class ArgumentsProvider
+	{
+		public static TArguments Parse<TArguments>(string[] args)
+			where TArguments : class
+		{
+			var parserResult = new Parser().Create<TArguments>(args);
+			return parserResult.Value;
+		}
+
+		public static void WriteErrorToConsole<TArguments>(string[] args)
+			where TArguments : class
+		{
+			var parserResult = new Parser().Create<TArguments>(args);
+
+			if(parserResult.Value != null)
+				return;
+
+			var errorMessage = new ParserSuggestionFormatter()
+				.CreateErrorMessage(
+					Environment.GetCommandLineArgs()[0],
+					parserResult.Suggestions);
+
+			new ConsoleStringRenderer()
+				.WriteLine(errorMessage);
+		}
+	}
+}
