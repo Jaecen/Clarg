f3e61f4019b2864ef834ab97a1c6cab9b50569b5
Jason Addington <jason@vortx.com>
2017-09-22 16:36:35 -0700

    Update to latest .Net Core Tooling

--- test/Clarg.Test/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Clarg.Tests")]
-[assembly: AssemblyDescription("Clarg test suite")]
-[assembly: AssemblyCompany("Vortx, Inc.")]
-[assembly: AssemblyProduct("Clarg")]
-[assembly: AssemblyCopyright("Copyright © Vortx, Inc. 2016")]
-
-[assembly: AssemblyVersion("2.0.1.*")]
